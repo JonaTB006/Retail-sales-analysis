16ecb7fe7109ac3955dbc3e73883a0f3aaf3bd2e
JonaTB006 <jonabska8@gmail.com>
2026-06-15 22:35:05 -0600

    Data model
    Data model

--- SQL/01_Data_Exploration.sql
+++ SQL/01_Data_Exploration.sql
@@ -39,7 +39,7 @@ FROM INFORMATION_SCHEMA.COLUMNS
 ORDER BY 1,2;
 
 /*------------------------------------------------------------
-4. Retrieve Keys in all tables
+5. Retrieve Keys in all tables
 ------------------------------------------------------------*/
 
 SELECT
@@ -52,3 +52,89 @@ ORDER BY TABLE_SCHEMA,
          TABLE_NAME,
          COLUMN_NAME;
 
+
+/*------------------------------------------------------------
+6. Retrieve tables in fact schema
+------------------------------------------------------------*/
+
+SELECT TABLE_NAME, TABLE_SCHEMA
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'FACT'
+ORDER BY 1;
+
+/*------------------------------------------------------------
+7. Retrieve table_name and it's colums in fact schema
+------------------------------------------------------------*/
+
+SELECT TABLE_NAME, COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'FACT'
+ORDER BY 1;
+
+/*------------------------------------------------------------
+8. Retrieve tables in dimension schema
+------------------------------------------------------------*/
+
+SELECT TABLE_NAME, COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'DIMENSION'
+ORDER BY 1;
+
+/*------------------------------------------------------------
+9. Retrieve table_name and it's colums in dimension schema
+------------------------------------------------------------*/
+
+SELECT TABLE_NAME, TABLE_SCHEMA
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'DIMENSION'
+ORDER BY 1;
+
+/*------------------------------------------------------------
+10. data tables exploration
+------------------------------------------------------------*/
+
+SELECT
+    COLUMN_NAME,
+    DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'Fact'
+  AND TABLE_NAME = 'Sale'
+ORDER BY ORDINAL_POSITION;
+
+
+SELECT
+    COLUMN_NAME,
+    DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'Dimension'
+  AND TABLE_NAME = 'Customer';
+
+SELECT
+    COLUMN_NAME,
+    DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'Dimension'
+  AND TABLE_NAME = 'city';
+
+
+SELECT
+    COLUMN_NAME,
+    DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'Dimension'
+  AND TABLE_NAME = 'date';
+
+
+SELECT 
+    COLUMN_NAME,
+    DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'Dimension'
+ AND TABLE_NAME = 'STOCK ITEM'
+
+SELECT TOP 10 *
+FROM Fact.[Sale]
+ORDER BY [Sale Key];
+
+SELECT TOP 10 *
+FROM Dimension.[Stock Item]
